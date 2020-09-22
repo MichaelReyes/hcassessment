109c98931049dccbd9743901bb200a1d87e3e56a
michaelangeloreyes <michaelangelo.reyes19@gmail.com>
2020-09-22 11:20:52 +0800

    [feature/weather_list] - Weather List * UI setup with Master Detail flow approach * updated styles, dimens and colors

--- app/src/main/java/com/example/hcassessment/core/network/AppApi.kt
+++ app/src/main/java/com/example/hcassessment/core/network/AppApi.kt
@@ -1,7 +1,15 @@
 package com.example.hcassessment.core.network
 
+import com.example.hcassessment.BuildConfig
+import com.example.hcassessment.core.data.pojo.group.WeatherGroupResponse
+import io.reactivex.Maybe
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface AppApi {
 
+    @GET("data/2.5/group?appid=${BuildConfig.OPEN_WEATHER_API_KEY}")
+    fun getWeatherGroups(@Query("id") ids: String): Maybe<WeatherGroupResponse>
 
 }
