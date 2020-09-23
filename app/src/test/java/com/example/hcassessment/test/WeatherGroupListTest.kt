08d00a521de3096c6c6828b6b44bcc6334402466
michaelangeloreyes <michaelangelo.reyes19@gmail.com>
2020-09-23 19:41:34 +0800

    [feature/unit_test] - Unit test
    ADD:
    * WeatherGroupListTest.kt
    * MockObjects.kt

---
+++ app/src/test/java/com/example/hcassessment/test/WeatherGroupListTest.kt
@@ -0,0 +1,135 @@
+package com.example.hcassessment.test
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.hcassessment.core.data.pojo.group.WeatherItem
+import com.example.hcassessment.core.network.WeatherRepository
+import com.example.hcassessment.feature.weather.group.WeatherGroupUi
+import com.example.hcassessment.feature.weather.group.WeatherGroupViewModel
+import com.example.hcassessment.utils.*
+import com.google.gson.Gson
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherGroupListTest {
+
+    @MockK
+    lateinit var context: Context
+
+    @MockK
+    lateinit var viewModel: WeatherGroupViewModel
+
+    @MockK(relaxed = true)
+    lateinit var repo: WeatherRepository
+
+    @MockK(relaxed = true)
+    lateinit var groupsObserver: Observer<List<WeatherItem>>
+
+    @MockK(relaxed = true)
+    lateinit var uiObserver: Observer<WeatherGroupUi>
+
+    val gson = Gson()
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val immediateScheduler: Scheduler = object : Scheduler() {
+
+        override fun createWorker() = ExecutorScheduler.ExecutorWorker(Executor { it.run() }, true)
+
+        // This prevents errors when scheduling a delay
+        override fun scheduleDirect(run: Runnable, delay: Long, unit: TimeUnit): Disposable {
+            return super.scheduleDirect(run, 0, unit)
+        }
+
+    }
+
+    @Before
+    fun setup() {
+
+        MockKAnnotations.init(this)
+
+        RxJavaPlugins.setIoSchedulerHandler { immediateScheduler }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediateScheduler }
+        RxAndroidPlugins.setMainThreadSchedulerHandler { immediateScheduler }
+
+        repo.apply {
+            every { getWeatherGroup(getCityIds().joinToString(separator = ",")) } returns getWeatherGroupsResponse()
+            every { getWeatherGroup(getCityIdsForException().joinToString(separator = ",")) } returns getWeatherGroupsExceptionResponse()
+            every { getWeatherDetails("1701668") } returns getManilaWeather()
+            every { getWeatherDetails("3067696") } returns getPragueWeather()
+            every { getWeatherDetails("1835848") } returns getSeoulWeather()
+        }
+
+        viewModel = WeatherGroupViewModel(context, repo)
+
+        viewModel.apply {
+            groups.observeForever(groupsObserver)
+            ui.observeForever(uiObserver)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        RxJavaPlugins.reset()
+        RxAndroidPlugins.reset()
+    }
+
+    @Test
+    fun `Fetch weather group API success`(){
+        viewModel.run {
+            fetchWeatherGroups(getCityIds())
+
+            val weatherGroupsCapture = slot<List<WeatherItem>>()
+
+            verify{ groupsObserver.onChanged(capture(weatherGroupsCapture)) }
+
+            assertEquals(weatherGroup.list, weatherGroupsCapture.captured)
+        }
+
+    }
+
+    @Test
+    fun `Fetch weather group API empty`(){
+        viewModel.run {
+            fetchWeatherGroups(emptyList())
+
+            val weatherGroupsCapture = slot<List<WeatherItem>>()
+
+            verify{ groupsObserver.onChanged(capture(weatherGroupsCapture)) }
+
+            assertEquals(emptyList<WeatherItem>(), weatherGroupsCapture.captured)
+        }
+
+    }
+
+    @Test
+    fun `Fetch weather group API with exception`(){
+        viewModel.run {
+            fetchWeatherGroups(getCityIdsForException())
+
+            val uiCapture = slot<WeatherGroupUi>()
+
+            verify{ uiObserver.onChanged(capture(uiCapture)) }
+
+            assertEquals("Ids not found", uiCapture.captured.error)
+        }
+
+    }
+}
---
+++ app/src/test/java/com/example/hcassessment/utils/MockObjects.kt
@@ -0,0 +1,26 @@
+package com.example.hcassessment.utils
+
+import com.example.hcassessment.core.data.pojo.group.WeatherGroupResponse
+import com.example.hcassessment.core.data.pojo.group.WeatherItem
+import com.example.hcassessment.core.extensions.fromJson
+import com.google.gson.Gson
+import io.reactivex.Maybe
+import io.reactivex.internal.operators.maybe.MaybeJust
+import java.lang.Exception
+
+
+val manilaWeather: WeatherItem = Gson().fromJson("{\"coord\":{\"lon\":120.98,\"lat\":14.6},\"weather\":[{\"id\":501,\"main\":\"Rain\",\"description\":\"moderate rain\",\"icon\":\"10n\"}],\"base\":\"stations\",\"main\":{\"temp\":301.54,\"feels_like\":306.63,\"temp_min\":300.93,\"temp_max\":302.15,\"pressure\":1009,\"humidity\":83},\"visibility\":9000,\"wind\":{\"speed\":2.1,\"deg\":190},\"rain\":{\"1h\":1.4},\"clouds\":{\"all\":90},\"dt\":1600858654,\"sys\":{\"type\":1,\"id\":8160,\"country\":\"PH\",\"sunrise\":1600811110,\"sunset\":1600854707},\"timezone\":28800,\"id\":1701668,\"name\":\"Manila\",\"cod\":200}")
+val praugeWeather: WeatherItem = Gson().fromJson("{\"coord\":{\"lon\":14.42,\"lat\":50.09},\"weather\":[{\"id\":804,\"main\":\"Clouds\",\"description\":\"overcast clouds\",\"icon\":\"04d\"}],\"base\":\"stations\",\"main\":{\"temp\":295.08,\"feels_like\":294.11,\"temp_min\":294.15,\"temp_max\":295.93,\"pressure\":1007,\"humidity\":60},\"visibility\":10000,\"wind\":{\"speed\":3.1,\"deg\":140},\"clouds\":{\"all\":93},\"dt\":1600859043,\"sys\":{\"type\":1,\"id\":6835,\"country\":\"CZ\",\"sunrise\":1600836633,\"sunset\":1600880319},\"timezone\":7200,\"id\":3067696,\"name\":\"Prague\",\"cod\":200}")
+val seoulWeather: WeatherItem = Gson().fromJson("{\"coord\":{\"lon\":126.98,\"lat\":37.57},\"weather\":[{\"id\":804,\"main\":\"Clouds\",\"description\":\"overcast clouds\",\"icon\":\"04n\"}],\"base\":\"stations\",\"main\":{\"temp\":293.28,\"feels_like\":291.49,\"temp_min\":292.15,\"temp_max\":294.15,\"pressure\":1017,\"humidity\":52},\"visibility\":10000,\"wind\":{\"speed\":2.6,\"deg\":40},\"clouds\":{\"all\":90},\"dt\":1600859000,\"sys\":{\"type\":1,\"id\":8117,\"country\":\"KR\",\"sunrise\":1600809638,\"sunset\":1600853299},\"timezone\":32400,\"id\":1835848,\"name\":\"Seoul\",\"cod\":200}")
+
+val weatherGroup: WeatherGroupResponse = WeatherGroupResponse(cnt = 3, list = listOf(manilaWeather, praugeWeather, seoulWeather))
+
+fun getCityIds(): List<String> = listOf("1701668","3067696","1835848")
+fun getCityIdsForException(): List<String> = listOf("12345","23456","345678")
+
+fun getWeatherGroupsResponse(): Maybe<WeatherGroupResponse> = MaybeJust(weatherGroup)
+fun getWeatherGroupsExceptionResponse(): Maybe<WeatherGroupResponse> = Maybe.error(Exception("Ids not found"))
+fun getManilaWeather() : Maybe<WeatherItem> = MaybeJust(manilaWeather)
+fun getPragueWeather() : Maybe<WeatherItem> = MaybeJust(praugeWeather)
+fun getSeoulWeather() : Maybe<WeatherItem> = MaybeJust(seoulWeather)
+
